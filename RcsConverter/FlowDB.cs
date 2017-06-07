756358e9a4e8e208c1bf5e965e84b6323af45ebe
Ams <adriansims62781@gmail.com>
2017-06-07 23:33:15 +0100

    Parse RJIS location file and find RCS file

---
+++ RcsConverter/FlowDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace RcsConverter
+{
+    class FlowDB
+    {
+        SQLiteConnection singleFlowDB;
+
+        /// <summary>
+        /// Cache of individual NLC dbs to write:
+        /// </summary>
+        LRUCache<string, SQLiteConnection> dbConnections = new LRUCache<string, SQLiteConnection>(50);
+        public FlowDB()
+        {
+            SQLiteConnection.CreateFile("RCSFlow.sqlite");
+            System.Collections.Generic.LinkedListNode<int> a;
+        }
+    }
+}
